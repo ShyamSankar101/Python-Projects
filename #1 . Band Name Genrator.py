3834cc9e4e681be8c256172c8342715c7f156cf5
ShyamSankar101 <110617574+ShyamSankar101@users.noreply.github.com>
2022-08-05 19:37:08 +0530

    day 1

---
+++ #1 . Band Name Genrator.py
@@ -0,0 +1,12 @@
+#1. Create a greeting for your program.
+print("Welcome to Band Name Genrator")
+
+#2. Ask the user for the city that they grew up in.
+city=input("Where did you grep up ?\t")
+
+#3. Ask the user for the name of a pet.
+pet_name=input("Your pet name ?\t")
+#4. Combine the name of their city and pet and show them their band name.
+print("your Band name is : " + city + pet_name)
+
+#5. Make sure the input cursor shows on a new line
